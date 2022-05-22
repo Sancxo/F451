8987709496d4f761ff3bc026344f07b9f53b795a
Simon <simon@7lieues.io>
2022-05-22 21:54:31 +0200

    build: integrating waffle library for file uploads

--- mix.exs
+++ mix.exs
@@ -48,7 +48,9 @@ defmodule F451.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      # Waffle: file upload library (https://github.com/elixir-waffle/waffle)
+      {:waffle, "~> 1.1"}
     ]
   end
 
